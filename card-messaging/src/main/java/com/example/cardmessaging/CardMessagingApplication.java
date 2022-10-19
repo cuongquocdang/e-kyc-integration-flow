43c7c624c83120eb89dbcfc4b756cace9aeafb2d
Cuong Quoc Dang <cuongdang.developer@gmail.com>
2022-10-19 14:29:47 +0700

    chore: init project

---
+++ card-messaging/src/main/java/com/example/cardmessaging/CardMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardMessagingApplication.class, args);
+    }
+
+}
